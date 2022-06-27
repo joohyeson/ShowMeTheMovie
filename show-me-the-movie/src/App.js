c351a3d3a0ee8d4abb4d6f9906a780cff729fff9
JuhyeSon <juhye.son990902@gmail.com>
2022-06-27 20:08:26 +0900

    영화 API와 연결, 기본 정보 화면 내에 띄움

--- show-me-the-movie/src/App.js
+++ show-me-the-movie/src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return (
-    <div><h1>Hello React!</h1></div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  
+  useEffect(() => {
+    getMovies();
+  }, [])
+  console.log(movies);
+  return (<div>{loading ? <h1>Loading..</h1> : <div>{movies.map((movie) => (
+    <div key={movies.id}>
+      <img src={movie.medium_cover_image}></img>
+      <h2>{movie.title}</h2>
+      <p>{movie.summary}</p>
+      <ul>
+        {movie.genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  ))}</div>}</div>);
 }
 
 export default App;
