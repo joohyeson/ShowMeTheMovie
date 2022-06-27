d54c9a750aa8480f7bfb61639bc56ae16e4d529a
JuhyeSon <juhye.son990902@gmail.com>
2022-06-27 22:19:55 +0900

    react-router-dom 모듈 추가 route주소에 따라 렌더링 되는 컴포넌트 분리

--- show-me-the-movie/src/App.js
+++ show-me-the-movie/src/App.js
@@ -1,27 +1,18 @@
-import { useEffect, useState } from "react";
-import Movie from './Movie'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    );
-
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  
-  useEffect(() => {
-    getMovies();
-  }, [])
-  console.log(movies);
-  return (<div>{loading ? <h1>Loading..</h1> : <div>{movies.map((movie) => (
-    <Movie
-      key={movie.id} medium_cover_image={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
-  ))}</div>}</div>);
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Detail/>}/>
+    </Routes>
+  </Router>
 }
 
 export default App;
