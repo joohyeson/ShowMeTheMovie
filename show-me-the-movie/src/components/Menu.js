0b7c6c370886b1b599bfcbab3c78e5f2e0d97694
JuhyeSon <juhye.son990902@gmail.com>
2022-06-30 12:45:01 +0900

    Navbar 추가

--- show-me-the-movie/src/components/Menu.js
+++ show-me-the-movie/src/components/Menu.js
@@ -1,10 +1,21 @@
-import styles from './Header.module.css'
+import styles from './Menu.module.css'
 
 
 function Menu() {
     return (
         <div className={styles.contents}>
-            hello
+            <ul>
+                <li>영화</li>
+                <li>극장</li>
+                <li>예매</li>
+                <li>스토어</li>
+                <li>이벤트</li>
+                <li>혜택</li>
+            </ul>
+            <ul className={styles.search}>
+                <li><input /></li>
+                <li><img src={process.env.PUBLIC_URL + '/public_assets/search.png'}/></li>
+            </ul>
         </div>
     )
 }
