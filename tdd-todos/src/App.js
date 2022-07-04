507092d0fa3de897afc967128e9efa8c2c6f9fd9
JuhyeSon <juhye.son990902@gmail.com>
2022-07-04 20:39:21 +0900

    TDD 연습용 폴더 생성

---
+++ tdd-todos/src/App.js
@@ -0,0 +1,11 @@
+import TodoForm from './TodoForm';
+
+function App() {
+  return (
+    <div>
+      <TodoForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tdd-todos/src/TodoForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TodoForm() {
+    return (
+        <div></div>
+    )
+}
+
+export default TodoForm;
