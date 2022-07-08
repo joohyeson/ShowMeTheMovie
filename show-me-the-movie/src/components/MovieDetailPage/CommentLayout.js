16538872e9400eb05bec8d653a2e55240bc33433
JuhyeSon <juhye.son990902@gmail.com>
2022-07-09 02:32:50 +0900

    /movies/detail-show 레이아웃 수정 및 선 추가

--- show-me-the-movie/src/components/MovieDetailPage/CommentLayout.js
+++ show-me-the-movie/src/components/MovieDetailPage/CommentLayout.js
@@ -7,27 +7,27 @@ class CommentLayout extends Component {
             <div className={style.contents}>
                 <h3>관객후기</h3>
             {/* 그리드 레이아웃만 설정 */}
-                <div className={style.grid}>
-                    <div style={{width:"100%"}}>
+                <ul className={style.grid}>
+                    <li style={{width:"100%"}}>
                     <Comment />
-                    </div>
-                    <div style={{width:"100%"}}>
+                    </li>
+                    <li style={{width:"100%"}}>
                     <Comment />
-                    </div>
-                    <div style={{width:"100%"}}>
+                    </li>
+                    <li style={{width:"100%"}}>
                     <Comment />
-                    </div>
-                    <div style={{width:"100%"}}>
+                    </li>
+                    <li style={{width:"100%"}}>
                     <Comment />
-                    </div>
-                    <div style={{width:"100%"}}>
+                    </li>
+                    <li style={{width:"100%"}}>
                     <Comment />
-                    </div>
-                    <div style={{width:"100%"}}>
+                    </li>
+                    <li style={{width:"100%"}}>
                     <Comment />
-                    </div>
+                    </li>
                 
-            </div>
+            </ul>
             </div>
         );
     }
